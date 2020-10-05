34ab6ec957ef0a00c14c7f05aff761bcbc81835a
Jordan Sinko <jordan5sinko@gmail.com>
2020-10-05 11:56:57 -0400

    test: adjusting tests to reflect actual usage

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,7 @@
 import "@aws-cdk/assert/jest";
-import { App, Stack } from "@aws-cdk/core";
+import { App, SecretValue, Stack } from "@aws-cdk/core";
+import { Artifact, Pipeline } from "@aws-cdk/aws-codepipeline";
+import { GitHubSourceAction } from "@aws-cdk/aws-codepipeline-actions";
 
 import { SlackApproval } from "../src";
 
@@ -7,8 +9,29 @@ test("Snapshot", () => {
   const app = new App();
   const stack = new Stack(app, "testing-stack");
 
+  const sourceArtifact = new Artifact();
+
   const approval = new SlackApproval(stack, "Approval", { slackToken: "xoxb-12345" });
-  approval.addApprovalAction("Approval", { actionName: "Approval", slackChannel: "1" });
+  new Pipeline(stack, "Pipeline", {
+    stages: [
+      {
+        stageName: "Source",
+        actions: [
+          new GitHubSourceAction({
+            actionName: "GitHub",
+            oauthToken: SecretValue.secretsManager("GITHUB_TOKEN"),
+            owner: "JordanSinko",
+            repo: "the-slack-pipeline-approval",
+            output: sourceArtifact,
+          }),
+        ],
+      },
+      {
+        stageName: "Approval",
+        actions: [approval.addApprovalAction("Approval", { actionName: "Approval", slackChannel: "1" })],
+      },
+    ],
+  });
 
   expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
 });
